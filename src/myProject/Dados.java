a745c11e4421f82ade3d7162bb4bde0f83d4d740
Brayan <leon16.bs@gmail.com>
2022-01-06 20:59:01 -0500

    Modificada la clase Dados

--- src/myProject/Dados.java
+++ src/myProject/Dados.java
@@ -9,31 +9,37 @@ import java.util.Random;
  * @version 1.0.0 fecha 3/1/2022
  */
 public class Dados {
-    /**
-     * Atributos de los dados
-     *
-     * Acciones:
-     * -> Meeple: permite relanzar otro dado en juego, es decir, de la sección dados activos.
-     * -> Nave espacial: envía un dado no usado (de la sección dados activos) a la sección de dados
-     * inactivos
-     * -> Dragón: es la cara que se quiere evitar, ya que si al final de la ronda es el último dado activo que
-     * queda se habrán perdido todos los puntos ganados y acumulados.
-     * -> Superhéroe: permite que cualquier dado no usado (sección dados activos) sea volteado y
-     * colocado en su cara opuesta.
-     * -> 42: es cara que permite sumar puntos al final de la ronda.
-     * -> Corazon:permite tomar un dado de la sección de dados inactivos y lanzarlo para que sea un
-     * nuevo dado activo.
-     */
-    private int acciones;
-    /**
-     * Metodo que genera el numero aleatorio entre 1 y 6 para las caras del dado
-     * @return valor de la carta
-     *
-     */
-    public int getAcciones() {
-        Random aleatorio = new Random();
-        acciones = aleatorio.nextInt(6)+1;
-        return acciones;
+    private int numero;
+    private int numeroAccion;
+    private String accion;
+
+    public Dados(int _numero){
+        numero = _numero;
+        numeroAccion = numeroAccion;
     }
 
+    public void setNumAccion(){
+        Random aleatorio = new Random();
+        numeroAccion = aleatorio.nextInt(6)+1;
+    }
+
+    public void setNumAccionNoAleatorio(int numero){
+        numeroAccion = numero;
+    }
+
+    public void setAccion(String _accion){
+        accion = _accion;
+    }
+
+    public int getNumAccion(){
+        return numeroAccion;
+    }
+
+    public String getAccion(){
+        return accion;
+    }
+
+    public int getNumero(){
+        return numero;
+    }
 }
